bc3a62e616b6aff68ff388a6d0cfc263c505897e
Jesus-Mejia232 <jesusmmarco00@gmail.com>
2023-02-22 09:14:36 -0600

    Actuali 22/02/23

--- archivos/escribiendo.py
+++ archivos/escribiendo.py
@@ -2,12 +2,10 @@
 with open("archivos\\Jesus.txt", "w", encoding="UTF-8") as archivo:
     # Reescribiendo el texto del archivo, #Con esta función podemos sobreescribir el texto del archivo "write"
     # Este metodo borra toda la información que tenga el archivo
-    archivo.writelines("Sobreescribiendo el texto del archivo ")
-    archivo.write("Algun texto por aquí\n")
+    # archivo.write("Sobreescribiendo el texto del archivo ")
     # Pero con el permiso "w" si no encuentra el archivo, lo crea
-    #Escribiendo sobre el archivo de texto con "writelines"
-    #archivo.writelines(["Hola jefe de jefes, como andas\n","Jesus"])
+    archivo.writelines("Hola jefe de jefes, como andás?")
     print(archivo)
-#Cuando está el permiso "w" sobre escribe lo que estaba en el archivo. Si se vuelve a usar
-#No elimina lo anteriormente escrito con la misma funcion sin importar el operador que vaya ("writelines" o 
-# "write")
+
+# Escribiendo con "writeline"
+#
--- archivos_remake/archivos_csv/leyendo_archivos/leyendo_csv_con_pandas.py
+++ archivos_remake/archivos_csv/leyendo_archivos/leyendo_csv_con_pandas.py
@@ -1,11 +1,15 @@
-#Para ver si tenemos instalada la libreria "pandas" iremos a cmd(como admistrador)
-#Y escribiremos las siguientes 3 alternativas:
-#1)"Pip" 2)"py pip" 3)"py -m pip"  si muestra que no está instalada la librería, a continuación veremos como
-#instalarla
-#Pasos, para actualizar es el siguiente comando "python -m ensurepip --upgrade" y para instalarlo es:
-#"python get-pip.py" o "py -m pip install pandas"
+# Para ver si tenemos instalada la libreria "pandas" iremos a cmd(como admistrador)
+# Y escribiremos las siguientes 3 alternativas:
+# 1)"Pip" 2)"py pip" 3)"py -m pip"  si muestra que no está instalada la librería, a continuación veremos como
+# instalarla
+# Pasos, para actualizar es el siguiente comando "python -m ensurepip --upgrade" y para instalarlo es:
+# "python get-pip.py" o "py -m pip install pandas"
 
 import pandas as pd
-
-archivo = pd.read_csv("archivos_remake\\archivos_csv\\leyendo_archivos\\datos.csv")
-print(archivo)
+# Se pone como nombre "df" para saber que se trata de un "dataframe"
+# Los dataframes, son estructuras de datos, bidimensionales similares a una hoja de calculo, al no
+# Comportarse como un archivo, tiene su propia forma de accederse. Siempre los valores que van a tener
+# Son 2 filas y columnas
+df = pd.read_csv("archivos_remake\\archivos_csv\\leyendo_archivos\\datos.csv")
+print(df["Nombre"])
+# Hay que ir familiarizandose con jupyter Notebook
