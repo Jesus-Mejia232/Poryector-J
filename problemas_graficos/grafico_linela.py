00190f9adeff5033bd27cd92635cd5d7432b521a
Jesus-Mejia232 <jesusmmarco00@gmail.com>
2023-03-03 16:42:11 -0600

    Actuali 03/03/23

--- problemas_graficos/grafico_linela.py
+++ problemas_graficos/grafico_linela.py
@@ -1,8 +1,14 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("problemas_graficos//pedos.csv")
+df = pd.read_csv("problemas_graficos\pedos.csv")
 
-sns.lineplot(x="fecha",y="pedos",data=df)
+# Creando el gráfico
+sns.lineplot(x="fecha", y="pedos", data=df)
+
+# Creando un punto en el momento de más pedos
+plt.plot("01-13", 23, "o")
+
+# Mostrando el gráfico
 plt.show()
