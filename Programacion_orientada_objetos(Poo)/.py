beffbf5cd00c5815c8c9424ab2a732d55e141ca7
Jesus-Mejia232 <jesusmmarco00@gmail.com>
2023-03-09 16:47:22 -0600

    Excepciones

---
+++ Programacion_orientada_objetos(Poo)/.py
@@ -0,0 +1,33 @@
+# Que es la programacion orientada a objetos?
+# Es basicamente otra manera más de programar.
+# Aqui tenemos las clases y los objetos, las clases son básicamente plantillas, y los objetos
+
+"""
+La programación orientada a objetos es un estilo de programacion que se centra en desarrollar software 
+basandonos en las clases y en los objetos, permite una mayor seguridad y encapsulamiento del código y también permite 
+reutirlizarlo
+
+
+La programación orientada a objetos se creo para programar cosas como si estuvieran pensadas para la vida real 
+Conceptos de programación orientada a objetos:
+1)Las clases: Es una especie de plantilla donde va a contener todas las caracteristicas que va a contener un objeto 
+
+2)Objeto: En programacion, es como una porcion de código, donde tomamos todo lo que está predefinido en la clase 
+
+3)Metodos: Los metodos son funciones que se aplican a un objeto, no son funciones, son metodos(sí son funciones), pero
+teoricamente se dice que son metodos. Se sabe que es un metodo cuando la funcion se aplica a un objeto }
+
+4)Propiedades: son las caracteristicas del objeto (tamaño,color,altura, etc...)
+
+5)Polimorfismo: Es la capacidad que tienen los objetos de comportarse de una manera difetente a un objeto, un ejemplo:
+se enciende una lampara, el metodo es "encender", la lampara blanca, lo que hace es largar luz blanca, la lampara verde
+lo que hace es largar luz verde. Aquí tenemos polimorfismo, porque ante el mismo método, se comportan de manera distinta
+Otro concepto de "polimorfismo" son objetos de una misma clase que se comportan de mandera distinta con el mismo metodo 
+Es la capacidad de comportarse de una manera diferente si le ponemos el mismo metodo 
+
+5)Herencia: Es la capacidad de ahorrar código en clases, heredasmos los métodos y las propiedades de la clase anteior
+cohecion
+acoplamiento 
+encapsulamiento 
+abstraccion
+"""
