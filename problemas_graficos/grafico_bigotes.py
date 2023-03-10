fa123ea4674c16084250a161cbdcac002d3ac198
jesus-mejia232 <jesusmmarco00@gmail.com>
2023-03-09 20:40:49 -0600

    Actuali

--- problemas_graficos/grafico_bigotes.py
+++ problemas_graficos/grafico_bigotes.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("problemas_graficos\\bigotes.csv")
+#Para leer archivos demasiado grandes, es mejor usar 
+#red_csv_in_chunk("archivos_grandes")
+
 
 # Creando el gráfico
 sns.boxplot(x="categoria", y="valor", data=df)
